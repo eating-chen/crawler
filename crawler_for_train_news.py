4c59bcc6a791d11a2c8be2ddc1251356f90419d3
eating-chen <ddoo8059@yahoo.com.tw>
2018-11-23 00:28:23 +0800

    new

--- crawler_for_train_news.py
+++ crawler_for_train_news.py
@@ -14,6 +14,28 @@ time_re = r"(\d{4}-\d{1,2}-\d{1,2}\s\d{1,2}:\d{1,2})"
 num_re = re.compile(u"[0-9]+")
 page_count = 1
 
+
+# 確認是否在資料庫有無重複
+def check_duplicate_in_mongodb(post_id, post_author, post_title, post_type):
+    if _social_network_msg.count({'post_id':post_id, 'post_author':post_author, 'post_title':post_title, 'post_type':post_type}) == 1:
+        return True
+    else:
+        return False
+
+def parse_post(post_link):
+    request = urllib.request.Request(post_link, headers=headers)
+    resp = urllib.request.urlopen(request)
+
+    url_content = str(resp.read().decode('utf-8'))
+    soup = BeautifulSoup(url_content, 'html.parser')
+    url = urllib.parse.urlparse(post_link)
+    param_of_link = urllib.parse.parse_qs(url.query)
+
+    entire_post = {}
+    
+    post_title = soup.find("title").get_text()
+    print(post_title)
+
 def crawler():
     for page in range(1, 3):
         url = base_url+"/"+str(page)
@@ -25,7 +47,8 @@ def crawler():
         news_title = soup.find_all("li", class_="rtddt")
         
         for title in news_title:
-            print(title.get_text())
+            news_link = title.find("a")
+            parse_post(news_link['href'])
 
 
 if __name__ == "__main__":
